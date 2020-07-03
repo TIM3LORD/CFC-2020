ea9818e7401e508a355e3cad5a135afc12ef5a4d
TIM3LORD <larisoncarvalho@gmail.com>
2020-07-03 14:56:36 +0530

    Added endpoints for Job postings Fixed and optimized 'get all' endpoint for users and jobs Added query parameters to get for get specific users and job listings Updated postman collection to reflect changes

--- BackEnd/flask_server.py
+++ BackEnd/flask_server.py
@@ -21,7 +21,8 @@ elif os.path.isfile('vcap-local.json'):
         password = creds['password']
         url = creds['host']
 
-databaseName = "databasedemo"
+userDatabaseName = "databasedemo"
+jobDatabaseName = "jobsdb"
 port = int(os.getenv('PORT', 8000))
 client = None
 db = None
@@ -29,10 +30,10 @@ app = Flask(__name__)
 
 client = Cloudant(user, password, url=url)
 client.connect()
-db = client[databaseName]
 
 @app.route('/v1.0/users', methods=['POST'])
 def post_user():
+    db = client[userDatabaseName]
     if not request.json:
         abort(400)
     document = db.create_document(request.json)
@@ -40,14 +41,74 @@ def post_user():
 
 @app.route('/v1.0/users/<id>', methods=['GET'])
 def get_user_by_id(id):
+    db = client[userDatabaseName]
     if id and id in db:
         return jsonify(db[id])
     else:
         return Response(status = 404)
 
 @app.route('/v1.0/users', methods=['GET'])
-def get_all_users():
-    return jsonify(db)
+def get_query_users():
+    db = client[userDatabaseName]
+    type = request.args.get('type')
+    email = request.args.get('email')
+    if type ==  "all":
+        result = []
+        for doc in db:
+            result.append(doc)
+        return jsonify(result)
+        
+    else:
+        selector = {}
+        if(type is not None and type != ""):
+            selector['type'] = {'$eq': type}
+        elif(email is not None and email != ""):
+            selector['emailAddress'] = {'$eq': email}
+        docs = db.get_query_result(selector)
+        result = []
+        for doc in docs:
+            result.append(doc)
+        return jsonify(result)
+
+##Job Postings
+@app.route('/v1.0/jobs', methods=['POST'])
+def post_job():
+    db = client[jobDatabaseName]
+    if not request.json:
+        abort(400)
+    document = db.create_document(request.json)
+    document.save()
+    return jsonify(document)
+
+@app.route('/v1.0/jobs/<id>', methods=['GET'])
+def get_job_by_id(id):
+    db = client[jobDatabaseName]
+    if id and id in db:
+        return jsonify(db[id])
+    else:
+        return Response(status = 404)
+
+@app.route('/v1.0/jobs', methods=['GET'])
+def get_query_jobs():
+    db = client[jobDatabaseName]
+    manufacturer = request.args.get('manufacturer')
+    location = request.args.get('location')
+    if manufacturer ==  "all":
+        result = []
+        for doc in db:
+            result.append(doc)
+        return jsonify(result)
+    else:
+        selector = {}
+        if(manufacturer is not None and manufacturer != ""):
+            selector['manufacturerId'] = {'$eq': manufacturer}
+        if(location is not None and location != ""):
+            selector['location'] = {'$eq': location}
+        docs = db.get_query_result(selector)
+        result = []
+        for doc in docs:
+            result.append(doc)
+        return jsonify(result)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
